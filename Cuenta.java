736bd9861865177ca263e1741f90306e8951498c
cristian <c.dorado@unimayor.edu.co>
2026-03-10 17:47:27 -0500

    feat: estos son mis aportes papu

---
+++ Cuenta.java
@@ -0,0 +1,21 @@
+abstract class Cuenta {
+    private final String numero;
+    private final String titular;
+    protected double saldo;
+
+    public Cuenta(String numero, String titular, double saldoInicial) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    public String getNumero() { return numero; }
+    public String getTitular() { return titular; }
+    public double getSaldo() { return saldo; }
+
+    public abstract boolean retirar(double monto);
+
+    public void depositar(double monto) {
+        saldo += monto;
+    }
+}
