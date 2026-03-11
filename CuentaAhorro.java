1060c8e415f73a0a547213f5d50d3edcde2af4ee
WilsonRuiz <w.ruiz@unimayor.edu.co>
2026-03-11 16:26:05 -0500

    feat: esta es mi correcion de las clases

--- CuentaAhorro.java
+++ CuentaAhorro.java
@@ -7,31 +7,19 @@ class CuentaAhorro extends Cuenta {
 
     @Override
     public boolean retirar(double monto) {
+        if (monto <= 0) {
+            System.out.println("El monto a retirar debe ser positivo.");
+            return false;
+        }
         if (monto <= saldo) {
             saldo -= monto;
             return true;
         }
+        System.out.println("Fondos insuficientes en cuenta de ahorro.");
         return false;
     }
 
     public void aplicarInteres() {
         saldo += saldo * INTERES_ANUAL;
-    }
-}
-
-class CuentaCorriente extends Cuenta {
-    private static final double LIMITE_SOBREGIRO = 500000;
-
-    public CuentaCorriente(String numero, String titular, double saldoInicial) {
-        super(numero, titular, saldoInicial);
-    }
-
-    @Override
-    public boolean retirar(double monto) {
-        if (monto <= saldo + LIMITE_SOBREGIRO) {
-            saldo -= monto;
-            return true;
-        }
-        return false;
     }
 }
